49584edb63b2d98aa289c0e410acbfebd124800d
Daniel Mabbett <support@emailclue.com>
2016-07-29 20:13:38 +1000

    Update deps

--- src/main/java/com/emailclue/api/model/CreateAccountResponse.java
+++ src/main/java/com/emailclue/api/model/CreateAccountResponse.java
@@ -1,11 +1,17 @@
 package com.emailclue.api.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CreateAccountResponse {
 
     private final Clue clue;
     private final String apiToken;
 
-    public CreateAccountResponse(String apiToken, Clue clue) {
+    @JsonCreator
+    public CreateAccountResponse(
+            @JsonProperty("apiToken") String apiToken,
+            @JsonProperty("clue") Clue clue) {
         this.apiToken = apiToken;
         this.clue = clue;
     }
